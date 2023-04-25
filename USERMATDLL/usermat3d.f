da0e5e3fe57da6375b82d15939c85a0b252e9f48
Andrey Tumanov <xxxfogxxx@gmail.com>
2023-04-25 22:31:37 +0300

    Check if no update

---
+++ USERMATDLL/usermat3d.f
@@ -0,0 +1,410 @@
+*deck,usermat3d    USERDISTRIB  parallel                                gal
+      subroutine usermat3d(
+     &                   matId, elemId,kDomIntPt, kLayer, kSectPt,
+     &                   ldstep,isubst,keycut,
+     &                   nDirect,nShear,ncomp,nStatev,nProp,
+     &                   Time,dTime,Temp,dTemp,
+     &                   stress,ustatev,dsdePl,sedEl,sedPl,epseq,
+     &                   Strain,dStrain, epsPl, prop, coords, 
+     &                   var0, defGrad_t, defGrad,
+     &                   tsstif, epsZZ, cutFactor, 
+     &                   var1, var2, var3, var4, var5,
+     &                   var6, var7)
+     
+!DEC$ ATTRIBUTES DLLEXPORT, ALIAS:"USERMAT3D"::usermat3d 
+c*************************************************************************
+c     *** primary function ***
+c
+c       The plasticity model is described by Ramberg-Osgood equation.
+c       The following subroutine contain a USERMAT subroutine for
+c       a plasticity model of 3D solid elements or plane elements
+c       in plane strain or axisymmetric stress state. 
+c
+c       If using this code for research or industrial purposes, please cite:
+c         // A.V. Tumanov, D.A. Kosov, D.I. Fedorenkov (2020)
+c             IMPLEMENTATION OF THE RAMBERG-OSGOOD STATE LAW IN 
+c                                  THE ANSYS FINITE ELEMENT SOFTWARE. 
+c                  Transactions of Academenergo, V1, P 44-61// 
+c         doi:10.34129/2070-4755-2020-58-1-44-61
+c
+c         User must define material constitutive law properly
+c         according to the stress state such as 3D, plane strain
+c         and axisymmetry.
+c         
+c*************************************************************************
+c     input arguments
+c     ===============
+c      matId     (int,sc,i)               material #
+c      elemId    (int,sc,i)               element #
+c      kDomIntPt (int,sc,i)               "k"th domain integration point
+c      kLayer    (int,sc,i)               "k"th layer
+c      kSectPt   (int,sc,i)               "k"th Section point
+c      ldstep    (int,sc,i)               load step number
+c      isubst    (int,sc,i)               substep number
+c      nDirect   (int,sc,in)              # of direct components
+c      nShear    (int,sc,in)              # of shear components
+c      ncomp     (int,sc,in)              nDirect + nShear
+c      nstatev   (int,sc,l)               Number of state variables
+c      nProp     (int,sc,l)               Number of material ocnstants
+c
+c      Temp      (dp,sc,in)               temperature at beginning of
+c                                         time increment
+c      dTemp     (dp,sc,in)               temperature increment 
+c      Time      (dp,sc,in)               time at beginning of increment (t)
+c      dTime     (dp,sc,in)               current time increment (dt)
+c
+c      Strain   (dp,ar(ncomp),i)          Strain at beginning of time increment
+c      dStrain  (dp,ar(ncomp),i)          Strain increment
+c      prop     (dp,ar(nprop),i)          Material constants defined by TB,USER
+c      coords   (dp,ar(3),i)              current coordinates
+c      defGrad_t(dp,ar(3,3),i)            Deformation gradient at time t
+c      defGrad  (dp,ar(3,3),i)            Deformation gradient at time t+dt
+c
+c     input output arguments              
+c     ======================             
+c      stress   (dp,ar(nTesn),io)         stress
+c      ustatev   (dp,ar(nstatev),io)      user state variable
+c            ustatev(1)                     - equivalent plastic strain
+c            ustatev(2) - statev(1+ncomp)   - plastic strain vector
+c            ustatev(nStatev)               - von-Mises stress
+c      sedEl    (dp,sc,io)                elastic work
+c      sedPl    (dp,sc,io)                plastic work
+c      epseq    (dp,sc,io)                equivalent plastic strain
+c      tsstif   (dp,ar(2),io)             transverse shear stiffness
+c                                         tsstif(1) - Gxz
+c                                         tsstif(2) - Gyz
+c                                         tsstif(1) is also used to calculate hourglass
+c                                         stiffness, this value must be defined when low
+c                                         order element, such as 181, 182, 185 with uniform 
+c                                         integration is used.
+c      var?     (dp,sc,io)                not used, they are reserved arguments 
+c                                         for further development
+c
+c     output arguments
+c     ================
+c      keycut   (int,sc,io)               loading bisect/cut control
+c                                         0 - no bisect/cut
+c                                         1 - bisect/cut 
+c                                         (factor will be determined by ANSYS solution control)
+c      dsdePl   (dp,ar(ncomp,ncomp),io)   material jacobian matrix
+c      epsZZ    (dp,sc,o)                 strain epsZZ for plane stress,
+c                                         define it when accounting for thickness change 
+c                                         in shell and plane stress states
+c      cutFactor(dp,sc,o)                 time step size cut-back factor 
+c                                         define it if a smaller step size is wished
+c                                         recommended value is 0~1
+c
+c*************************************************************************
+c
+c      ncomp   6   for 3D  (nshear=3)
+c      ncomp   4   for plane strain or axisymmetric (nShear = 1)
+c
+c      stresss and strains, plastic strain vectors
+c          11, 22, 33, 12, 23, 13    for 3D
+c          11, 22, 33, 12            for plane strain or axisymmetry
+c
+c      material jacobian matrix
+c        3D
+c           dsdePl    |  1111   1122   1133   1112   1123   1113 |
+c           dsdePl    |  2211   2222   2233   2212   2223   2213 |
+c           dsdePl    |  3311   3322   3333   3312   3323   3313 |
+c           dsdePl    |  1211   1222   1233   1212   1223   1213 |
+c           dsdePl    |  2311   2322   2333   2312   2323   2313 |
+c           dsdePl    |  1311   1322   1333   1312   1323   1313 |
+c        plane strain or axisymmetric (11, 22, 33, 12)
+c           dsdePl    |  1111   1122   1133   1112 |
+c           dsdePl    |  2211   2222   2233   2212 |
+c           dsdePl    |  3311   3322   3333   3312 |
+c           dsdePl    |  1211   1222   1233   1212 |
+c
+c*************************************************************************
+#include "impcom.inc"
+c
+      INTEGER          
+     &                 matId, elemId,
+     &                 kDomIntPt, kLayer, kSectPt,
+     &                 ldstep,isubst,keycut,
+     &                 nDirect,nShear,ncomp,nStatev,nProp
+      DOUBLE PRECISION 
+     &                 Time,    dTime,   Temp,    dTemp,
+     &                 sedEl,   sedPl,   epseq,   epsZZ,   cutFactor
+      DOUBLE PRECISION 
+     &                 stress  (ncomp  ), ustatev (nStatev),
+     &                 dsdePl  (ncomp,ncomp), 
+     &                 Strain  (ncomp  ), dStrain (ncomp  ), 
+     &                 epsPl   (ncomp  ), prop    (nProp  ), 
+     &                 coords  (3),
+     &                 defGrad (3,3),     defGrad_t(3,3),
+     &                 tsstif  (2)
+c
+c***************** User defined part *************************************
+c
+c --- parameters
+c
+      INTEGER          mcomp
+      DOUBLE PRECISION HALF, THIRD, ONE, TWO, SMALL, ONEHALF,
+     &                 ZERO, TWOTHIRD, ONEDM02, ONEDM05, sqTiny
+      PARAMETER       (ZERO       = 0.d0,
+     &                 HALF       = 0.5d0,
+     &                 THIRD      = 1.d0/3.d0,
+     &                 ONE        = 1.d0,
+     &                 TWO        = 2.d0,
+     &                 SMALL      = 1.d-08,
+     &                 sqTiny     = 1.d-20,
+     &                 ONEDM02    = 1.d-02,
+     &                 ONEDM05    = 1.d-05,
+     &                 ONEHALF    = 1.5d0,
+     &                 TWOTHIRD   = 2.0d0/3.0d0,
+     &                 mcomp      = 6
+     &                 )
+c
+c --- user local variables
+c
+c      sigElp   (dp,ar(6  ),l)            trial stress
+c      dsdeEl   (dp,ar(6,6),l)            elastic moduli
+c      sigDev   (dp,ar(6  ),l)            deviatoric stress tensor
+c      dfds     (dp,ar(6  ),l)            derivative of the yield function 
+c      JM       (dp,ar(6,6),l)            2D matrix for a 4 order tensor
+c      pEl      (dp,sc     ,l)            hydrostatic pressure stress
+c      qEl      (dp,sc     ,l)            von-mises stress
+c      pleq_t   (dp,sc     ,l)            equivalent plastic strain at beginnig of time increment
+c      pleq     (dp,sc     ,l)            equivalent plastic strain at end of time increment
+c      dpleq    (dp,sc     ,l)            incremental equivalent plastic strain
+c      sigy     (dp,sc     ,l)            current equivalent stress at end of time increment
+c      young    (dp,sc     ,l)            Young's modulus
+c      posn     (dp,sc     ,l)            Poiss's ratio
+c      sigy0    (dp,sc     ,l)            initial yield stress
+c      dsigdep  (dp,sc     ,l)            plastic slop
+c      twoG     (dp,sc     ,l)            two time of shear moduli
+c      threeG   (dp,sc     ,l)            three time of shear moduli
+c      Strainob (dp,ar(ncomp),i           total strains
+c      alfa     (dp,sc     ,l)            strain hardening constant
+c      n        (dp,sc     ,l)            strain hardening exponent
+c      sigeqv   (dp,sc     ,l)            equivalent elastic stress
+c      epseqv   (dp,sc     ,l)            equivalent strian
+c      f1, f2, x, x1, x2, sch (dp,sc,l)   variables used to determine actual stress
+c      eps      (dp,sc     ,l)            iteration precision
+c      
+
+      EXTERNAL         vzero, vmove, get_ElmData, egen
+      double precision egen
+      DOUBLE PRECISION sigElp(mcomp), Strainob(mcomp),
+     &                 dsdeEl(mcomp,mcomp), G(mcomp),
+     &                 sigDev(mcomp), JM    (mcomp,mcomp), dfds(mcomp),
+     &                 sigi  (mcomp), strainEl(mcomp), ak(mcomp)
+
+      DOUBLE PRECISION var0, var1, var2, var3, var4, var5,
+     &                 var6, var7
+
+      DATA G/1.0D0,1.0D0,1.0D0,0.0D0,0.0D0,0.0D0/
+c
+      INTEGER          i, j, sch, urt
+      DOUBLE PRECISION pEl,   qEl, sigy,
+     &                 dpleq, pleq, 
+     &                 young, posn,    sigy0,   dsigdep, 
+     &                 elast1,elast2, alfa, n, 
+     &                 twoG,  threeG,  oneOv3G, qElOv3G, threeOv2qEl, 
+     &                 fratio,  con1,  con2, dperr(3), eps, lamme, emd,
+     &                 x, x1, x2, f1, f2, young1, posn1, sigeqv, epseqv
+c      
+c*************************************************************************
+      keycut   = 0
+c
+c *** get Young's modulus and Poisson's ratio, initial yield stress and the hardening parameters
+      young    = prop(1)
+      posn     = prop(2)
+      sigy0    = prop(3)
+      alfa     = prop(4)
+      n        = prop(5)
+c
+c *** calculate elastic stiffness matrix (3d)
+      twoG     = young / (ONE+posn)
+      threeG   = ONEHALF * twoG
+      elast1=young*posn/((1.0D0+posn)*(1.0D0-TWO*posn))
+      elast2=HALF*twoG
+c
+      dsdeEl(1,1)=(elast1+TWO*elast2)*G(1)*G(1)
+      dsdeEl(1,2)=elast1*G(1)*G(2)+elast2*TWO*G(4)*G(4)
+      dsdeEl(1,3)=elast1*G(1)*G(3)+elast2*TWO*G(5)*G(5)
+      dsdeEl(1,4)=elast1*G(1)*G(4)+elast2*TWO*G(1)*G(4)
+      dsdeEl(1,5)=elast1*G(1)*G(5)+elast2*TWO*G(1)*G(5)
+      dsdeEl(1,6)=elast1*G(1)*G(6)+elast2*TWO*G(4)*G(5)
+      dsdeEl(2,2)=(elast1+TWO*elast2)*G(2)*G(2)
+      dsdeEl(2,3)=elast1*G(2)*G(3)+elast2*TWO*G(6)*G(6)
+      dsdeEl(2,4)=elast1*G(2)*G(4)+elast2*TWO*G(1)*G(4)
+      dsdeEl(2,5)=elast1*G(2)*G(5)+elast2*TWO*G(1)*G(5)
+      dsdeEl(2,6)=elast1*G(2)*G(6)+elast2*TWO*G(2)*G(6)
+      dsdeEl(3,3)=(elast1+TWO*elast2)*G(3)*G(3)
+      dsdeEl(3,4)=elast1*G(3)*G(4)+elast2*TWO*G(5)*G(6)
+      dsdeEl(3,5)=elast1*G(3)*G(5)+elast2*TWO*G(5)*G(3)
+      dsdeEl(3,6)=elast1*G(3)*G(6)+elast2*TWO*G(6)*G(3)
+      dsdeEl(4,4)=elast1*G(4)*G(4)+elast2*(G(1)*G(2)+G(4)*G(4))
+      dsdeEl(4,5)=elast1*G(4)*G(5)+elast2*(G(1)*G(6)+G(5)*G(4))
+      dsdeEl(4,6)=elast1*G(4)*G(6)+elast2*(G(4)*G(6)+G(5)*G(2))
+      dsdeEl(5,5)=elast1*G(5)*G(5)+elast2*(G(1)*G(3)+G(5)*G(5))
+      dsdeEl(5,6)=elast1*G(5)*G(6)+elast2*(G(4)*G(3)+G(5)*G(6))
+      dsdeEl(6,6)=elast1*G(6)*G(6)+elast2*(G(2)*G(3)+G(6)*G(6))
+      do i=1,ncomp-1
+        do j=i+1,ncomp
+          dsdeEl(j,i)=dsdeEl(i,j)
+        end do
+      end do
+c
+c *** define tsstif(1) since it is used for calculation of hourglass stiffness
+      tsstif(1) = elast2
+c
+c *** get initial stress
+      call vzero(sigi(1),ncomp)
+      i = ncomp
+      call get_ElmData ('ISIG', elemId,kDomIntPt, i, sigi)
+c
+c *** calculate the trial stress and
+c     copy elastic moduli dsdeEl to material Jacobian matrix
+      do i=1,ncomp
+         strainEl(i) = Strain(i) + dStrain(i)
+         Strainob(i) = Strain(i) + dStrain(i) - epsPl(i)
+      end do
+      call vzero(sigElp, 6)
+      do i=1,ncomp
+         do j=1,ncomp
+            dsdePl(j,i) = dsdeEl(j,i)
+            sigElp(i) = sigElp(i)+dsdeEl(j,i)*strainEl(j)
+         end do
+c         sigElp(i) = sigElp(i) + sigi(i)
+      end do
+c
+c *** hydrostatic pressure stress
+      pEl = -THIRD * (sigElp(1) + sigElp(2) + sigElp(3))
+      
+c *** compute the deviatoric stress tensor
+      sigDev(1) = sigElp(1) + pEl
+      sigDev(2) = sigElp(2) + pEl
+      sigDev(3) = sigElp(3) + pEl
+      sigDev(4) = sigElp(4)
+      sigDev(5) = sigElp(5)
+      sigDev(6) = sigElp(6)
+c
+c *** compute von-mises and equivalent elastic stress
+      qEl = 
+     &  sigDev(1) * sigDev(1)+sigDev(2) * sigDev(2)+
+     &  sigDev(3) * sigDev(3)+
+     &  TWO*(sigDev(4) * sigDev(4)+ sigDev(5) * sigDev(5)+ 
+     &  sigDev(6) * sigDev(6))
+      qEl = sqrt( ONEHALF * qEl)
+      sigeqv = qEl
+c
+c *** compute equivalent strian
+      epseqv = 
+     &         (((strainEl(1)-strainEl(2))**2+(strainEl(2)-
+     &         strainEl(3))**2+(strainEl(3)-strainEl(1))**2 +
+     &         2/3*(strainEl(4)**2+strainEl(5)**2+
+     &         strainEl(6)**2))*1/2)**HALF/(1+posn)
+c
+c *** calculate the actual stress      
+        if (all(dstrain .EQ. 0))  GO TO 500
+            f1 = 0
+            f2 = 0
+            x = 0
+            eps = 1e-10
+            sch = 0
+            x1 = sigeqv
+            x2 = 
+     &         x1-(epseqv-(x1/young+alfa*sigy0/young*(x1/sigy0)**n))/
+     &         (1/young+alfa*n/young*sigy0 **(1-n)*x1**(n-1))
+            If (abs(x1-x2)<eps) then
+                sigeqv = x2
+            else
+                do while (abs(x2-x1).ge.eps)
+                f1 = x1/young+alfa/young*sigy0 **(1-n)*x1**n-epseqv
+                f2 = x2/young+alfa/young*sigy0 **(1-n)*x2**n-epseqv
+                x = x2-((x2-x1)*f2)/(f2-f1)
+                x1 = x2
+                x2 = x 
+                sch = sch+1
+                IF (sch>1000) GO TO 700
+                end do
+            sigeqv = x  
+      end if
+c
+c *** compute current yield stress
+      sigy = sigeqv      
+      threeOv2qEl = ONEHALF/qEl
+c
+c *** compute derivative of the yield function
+      fratio = qEl / sigy - ONE
+c
+      threeOv2qEl = ONEHALF / qEl
+c *** compute derivative of the yield function
+      DO i=1, ncomp
+         dfds(i) = threeOv2qEl * sigDev(i)
+      END DO
+      oneOv3G  = ONE / threeG
+      qElOv3G  = qEl * oneOv3G
+      
+c *** initial guess of incremental equivalent plastic strain
+      pleq     = (qEl - sigy) * oneOv3G     
+      dpleq    = pleq - epseq
+      dsigdep  = sigy/pleq
+      
+c ***  update stresses
+      DO i = 1 , ncomp
+      stress(i) =  sigElp(i) - TWOTHIRD * (qEl-sigy) * dfds(i)  
+      END DO
+c
+c ***  update plastic strains
+      DO i = 1 , nDirect
+         epsPl(i) = epsPl(i) + dfds(i) * dpleq
+      END DO
+      DO i = nDirect + 1 , ncomp
+         epsPl(i) = epsPl(i) + TWO * dfds(i) * dpleq      
+      END DO
+      epseq  = pleq
+c
+c *** Material Jcobian matrix
+      IF (qEl.LT.sqTiny) THEN
+         con1 = ZERO
+      ELSE
+         con1 = threeG * dpleq / qEl
+      END IF
+      con2 = threeG/(threeG+dsigdep) - con1
+      con2 = TWOTHIRD * con2 
+      DO i=1,ncomp
+         DO j=1,ncomp
+            JM(j,i) = ZERO
+         END DO
+      END DO
+      DO i=1,nDirect
+         DO j=1,nDirect
+            JM(i,j) = -THIRD
+         END DO
+         JM(i,i) = JM(i,i) + ONE
+      END DO
+      DO i=nDirect + 1,ncomp
+         JM(i,i) = HALF
+      END DO
+      DO i=1,ncomp
+         DO j=1,ncomp
+            dsdePl(i,j) =    dsdeEl(i,j) - twoG
+     &           * (  con2 * dfds(i) * dfds(j) + con1 * JM(i,j) )   
+         END DO
+      END DO
+c
+      sedEl = ZERO
+c     
+c *** Calculate elastic work
+      DO i = 1 , ncomp
+         sedEl = sedEl + stress(i)*(Strain(i)+dStrain(i)-epsPl(i))
+      END DO
+      sedEl    = sedEl * HALF
+c
+c     Plastic strain energy density
+      sedPL = (1+posn)/3/young*(sigy**2)
+      sedPL=sedPL+1.5*(1-2*posn)/young*(pEl**2)
+      sedPL=sedPL+alfa*n/(n+1)/(young*sigy0**(n-1))*(sigy**(n+1))
+      sedPL=sedPL-sedEl 
+c
+  500 continue      
+c
+      return
+  700 continue      
+      end
